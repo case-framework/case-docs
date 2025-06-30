66de58b33429bf0ce7038475bee0770abe73a0cd
phev8 <phev8@users.noreply.github.com>
2025-06-30 15:55:07 +0200

    Enhance MDX components with ImageZoom support, update global styles to use black.css, replace logo SVG with Image component, modify homepage title to 'CASE Documentation', and add breadcrumb support in DocsPage. Remove obsolete index.mdx file.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,15 @@
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { ImageProps } from 'next/image';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: (props) => <ImageZoom {...(props as ImageProps)}
+      className='p-2 bg-neutral-200 rounded-xl'
+    />,
     ...components,
   };
 }
