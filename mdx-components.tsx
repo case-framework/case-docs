0ec32ee383f39fe5d8fe900625535604cbe58257
phev8 <phev8@users.noreply.github.com>
2025-09-03 17:46:16 +0200

    add LocaleVideo mdx comp

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,7 @@ import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { ImageProps } from 'next/image';
+import LocaleVideo from './components/content-items/locale-video';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -12,6 +13,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
       width={props.width || 700} // standard height if not defined in mdx
       height={props.height || 350} // standard height if not defined in mdx
     />,
+    LocaleVideo: (props) => <LocaleVideo {...(props as LocaleVideoProps)} />,
+
     ...components,
   };
 }
