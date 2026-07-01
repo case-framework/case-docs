8b3ff05ade667c1f652c2448949c94cbad1dcee9
kschneider84 <77678452+kschneider84@users.noreply.github.com>
2026-07-01 15:04:06 +0200

    remove unused imports

--- lib/source-docs.ts
+++ lib/source-docs.ts
@@ -1,7 +1,7 @@
 import { docs } from 'fumadocs-mdx:collections/server';
-import { loader, multiple } from 'fumadocs-core/source';
+import { loader } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
-import { openapiPlugin, openapiSource } from 'fumadocs-openapi/server';
+import { openapiPlugin } from 'fumadocs-openapi/server';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader(
